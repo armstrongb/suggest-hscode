f01a33c6ae00556d602e49402d60e3ea5033d02b
Ben Armstrong <ben.armstrong@trade.gov.uk>
2022-10-11 15:57:43 +0100

    Move data location and add copy to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 import gulp from 'gulp';
 import definePrototypeKitGulpTasks from '@ons/prototype-kit/defineGulpTasks.js';
 
+const { src, dest } = require('gulp');
+
+exports.default = function() {
+  return src('src/data/*.json')
+    .pipe(dest('build/data/'));
+}
+
 definePrototypeKitGulpTasks(gulp);
