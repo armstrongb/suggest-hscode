f0b50d7a8636dae847ee3cce82e381849dce0489
Ben Armstrong <ben.armstrong@trade.gov.uk>
2022-10-12 22:09:47 +0100

    Template config update, local styling sass, location file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 import gulp from 'gulp';
 import definePrototypeKitGulpTasks from '@ons/prototype-kit/defineGulpTasks.js';
 
-const { src, dest } = require('gulp');
-
-exports.default = function() {
-  return src('src/data/*.json')
-    .pipe(dest('build/data/'));
-}
 
 definePrototypeKitGulpTasks(gulp);
